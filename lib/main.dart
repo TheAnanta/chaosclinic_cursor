6bc8e858584b91a94161abb5dedf3bb30947138c
Manas Malla <manasmalla.dev@gmail.com>
2025-09-16 03:12:09 +0530

    Add Firebase configuration and initialize in main application

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'core/dependency_injection.dart';
+import 'firebase_options.dart';
 import 'ui/core/themes/app_theme.dart';
 import 'ui/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Initialize Firebase
-  await Firebase.initializeApp();
-  
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const ChaosClinicApp());
 }
 
